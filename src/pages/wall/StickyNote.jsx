d0c8a79037ca4716b57e9ec59df2eea8503cc94a
MARVYN Hoang <duong.hoang@LAP-162.local>
2023-01-24 15:24:02 +0700

    simply sticky notes

--- src/pages/wall/StickyNote.jsx
+++ src/pages/wall/StickyNote.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+const utilizeFocus = () => {
+    const ref = React.createRef()
+    const setFocus = () => {ref.current &&  ref.current.focus()}
+
+    return {setFocus, ref} 
+}
 
 export default class StickyNote extends React.Component {
     constructor(props) {
         super(props);
+        this.inputFocus = utilizeFocus()
         this.state = {
-            title: this.props.title,
             text: this.props.text,
         }
     }
@@ -15,26 +21,53 @@ export default class StickyNote extends React.Component {
         })
     }
 
-    onChangeTitle = (title) => {
-        this.setState({
-            title: title
-        })
+    focusNote = () => {
+        this.inputFocus.setFocus()
+    }
+
+    selectRange = (e) => {
+        let curText = e.currentTarget.innerText;
+        e.currentTarget.innerText = ''
+        e.currentTarget.innerText = curText;
+        if(e.target.innerText && document.createRange)
+        {
+            let range = document.createRange();
+            let selection = window.getSelection();
+            range.selectNodeContents(e.currentTarget);
+            range.setStart(e.currentTarget.firstChild,e.currentTarget.innerText.length);
+            range.setEnd(e.currentTarget.firstChild,e.currentTarget.innerText.length);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
+    saveNote = () => {
+
     }
 
     render() {
-        const { backgroundColor, rotate, left, top } = this.props;
-        const { title, text } = this.state;
+        const { backgroundColor, rotate, readOnly } = this.props;
+        const { text } = this.state;
         return (
-            <div className="sticky-note"
-                style={{ background: backgroundColor, transform: `rotate(${rotate}deg)`, left: left, top: top }}>
-                <h2
+            <div className="sticky-note" onClick={this.focusNote} 
+                onBlur={this.saveNote}
+                style={{ background: backgroundColor, transform: `rotate(${rotate}deg)`}}>
+                <p  ref={this.inputFocus.ref}
+                    onFocus={this.selectRange}
+                    onKeyDown={e => {
+                        if(e.currentTarget.innerText.length >= 100) {
+                            e.preventDefault();
+                            return;
+                        }
+                    }}
+                    onKeyUp={e => {
+                        if(e.currentTarget.innerText.length >= 100) {
+                            e.preventDefault();
+                            return;
+                        }
+                    }}
                     onBlur={e => this.onChangeText(e.target.innerText)}
-                    contentEditable={true} suppressContentEditableWarning={true} className="title">{title}
-                </h2>
-                <p
-                    // onInput={e => this.onChangeText(e.currentTarget.textContent)}
-                    onBlur={e => this.onChangeText(e.target.innerText)}
-                    contentEditable={true} suppressContentEditableWarning={true} className="text">{text}
+                    contentEditable={!readOnly} suppressContentEditableWarning={true} className="text">{text}
                 </p>
             </div>
         )
--- src/pages/wall/Wall.jsx
+++ src/pages/wall/Wall.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import StickyNote from "./StickyNote";
-
+const bColor = ["#ffc", "#ccf", "#cfc", "#ffcce2", "#ccfff4", "#ffccf2", "#ccf8ff", "#f55f5f", "#a9f55f", "#5ff5e5",
+"#5fa8f5", "#bb5ff5"]
+const rangeRotate = [-10, 10]
 export default class Wall extends React.Component{
+    constructor(props) {
+        super(props);
+        this.statet = {
 
-    componentDidMount() {
-        
+        }
     }
 
     render() {
         return(
             <div className="wall-container">
+                <StickyNote text="write something for me with limit hundred characters" readOnly={true}
+                    backgroundColor="#ffc" rotate={-6} />
                 <StickyNote title="Title1" text="Text content A"
-                    backgroundColor="#ffc" rotate={-6} left="70%" top="30%" />
+                    backgroundColor="#ccf" rotate={10} left="0%" top="0%" />
                 <StickyNote title="Title1" text="Text content A"
-                    backgroundColor="#ccf" rotate={10} left="40%" top="15%" />
+                    backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
                 <StickyNote title="Title1" text="Text content A"
-                    backgroundColor="#cfc" rotate={5} left="17%" top="60%" />
+                    backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
+                <StickyNote title="Title1" text="Text content A"
+                backgroundColor="#cfc" rotate={5} left="0%" top="0%" />
             </div>
         )
     }
