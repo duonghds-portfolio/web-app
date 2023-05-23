e5073b50e87c7abfa96a069b17fae3d5427eec96
duonghd <duonghds@github.com>
2023-05-24 00:39:43 +0700

    update new resume

--- src/pages/home/Resume.jsx
+++ src/pages/home/Resume.jsx
@@ -2,7 +2,7 @@ const Resume = () => {
     return(
         <a className="resume" target="_blank" rel="noreferrer"
         //ref this: https://bobbyhadz.com/blog/aws-s3-allow-public-read-access
-        href="https://duonghds-portfolio.s3.ap-southeast-1.amazonaws.com/public/HoangDuong_Resume_20230107.pdf"
+        href="https://duonghds-portfolio.s3.ap-southeast-1.amazonaws.com/public/HoangDuong_Resume_230524.pdf"
         >Resume</a>
     )
 }
